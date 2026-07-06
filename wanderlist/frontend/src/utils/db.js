1328c741f2dc7fd3cdd40f9015d47d8b29e192cf
adigeronimo02 <a.digeronimo02@icloud.com>
2026-07-06 17:12:48 +0200

    add

---
+++ wanderlist/frontend/src/utils/db.js
@@ -0,0 +1,10 @@
+import Dexie from 'dexie'
+
+export const db = new Dexie('wanderlist')
+
+db.version(1).stores({
+  cities:    '++id, name',
+  places:    '++id, city_id, type, category, tag',
+  ratings:   '++id, [place_id+user], place_id',
+  favorites: '++id, [user+place_id], user, place_id',
+})
